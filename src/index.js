e08012a707b1aee0bead48581284979ca10a4170
SAW <99191294+nicsaw@users.noreply.github.com>
2024-02-07 04:07:03 +0700

    Command handling

--- src/index.js
+++ src/index.js
@@ -29,9 +29,26 @@ client.on(Events.ClientReady, (c) => {
   console.log(`🟢 ${c.user.username} is online.`);
 });
 
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  console.log(interaction);
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
