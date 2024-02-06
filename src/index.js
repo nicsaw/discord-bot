f0e9b3d918d5c78f6a0cd834e47c069bf38f126f
SAW <99191294+nicsaw@users.noreply.github.com>
2024-02-07 04:46:48 +0700

    Add event handler

--- src/index.js
+++ src/index.js
@@ -25,30 +25,16 @@ for (const file of commandFiles) {
   }
 }
 
-client.on(Events.ClientReady, (c) => {
-  console.log(`🟢 ${c.user.username} is online.`);
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
+}
 
 client.login(process.env.BOT_TOKEN);
