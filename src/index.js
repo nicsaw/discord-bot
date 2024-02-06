af78c6257436664d252dfafdc1d326ee53dc8bb1
SAW <99191294+nicsaw@users.noreply.github.com>
2024-02-07 03:55:13 +0700

    Get all command files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+const { Client, IntentsBitField, Collection, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -9,14 +11,27 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent,
   ],
 });
+client.commands = new Collection();
 
-client.on('ready', (c) => {
+const foldersPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(foldersPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const filePath = path.join(foldersPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
+}
+
+client.on(Events.ClientReady, (c) => {
   console.log(`🟢 ${c.user.username} is online.`);
 });
 
-client.on('messageCreate', (message) => {
-  if (message.author.bot) return;
-  message.reply('hi');
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
 });
 
 client.login(process.env.BOT_TOKEN);
